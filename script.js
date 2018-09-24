2c7abf7f7894bfa798810965cd4ef8b5dc20728f
Jakub Kraśnicki <jakubkrasnickii@gmail.com>
2018-09-24 17:13:36 +0200

    Moved the script from html file to script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-
+ function removeTransition(e) {
+    this.classList.remove('playing');
+  }
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    audio.currentTime = 0;  //"overwrites" sound on multiple keydowns
+    audio.play();
+    key.classList.add('playing'); //transition
+  }
+  window.addEventListener('keydown', playSound);
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition))
